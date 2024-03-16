a6fa73e021ffa7ba6f2ce5c7bdffc7eaafa47ede
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-03-16 16:23:49 -0300

    Propriedades: Entendendo seu conceito

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Post } from './Post';
 
 export function App() {
-
-  return (
-    <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
-  )
+	return (
+		<div>
+			<Post
+				author="Alexandro do Nascimento Rossi"
+				content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque doloremque, officiis, numquam architecto ducimus iste laborum atque quae animi ad fuga? Id quos veniam perspiciatis ea harum nihil tenetur fugiat."
+			/>
+		</div>
+	)
 }

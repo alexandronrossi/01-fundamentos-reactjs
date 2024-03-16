18aaaadb77928e0986a3fd82cff38c0f65f1e4a8
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-03-16 19:01:02 -0300

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
 
 import './global.css';
@@ -5,6 +6,8 @@ import './global.css';
 export function App() {
 	return (
 		<div>
+			<Header />
+
 			<Post
 				author="Alexandro do Nascimento Rossi"
 				content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque doloremque, officiis, numquam architecto ducimus iste laborum atque quae animi ad fuga? Id quos veniam perspiciatis ea harum nihil tenetur fugiat."

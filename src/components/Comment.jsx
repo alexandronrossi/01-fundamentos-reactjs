2d786c71b2c07927f36cb32509755ba2a5d254e8
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-04 13:48:12 -0300

    Realizando likes nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,8 +3,16 @@ import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from 'phosphor-react';
 
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({content, deleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment()
+    {
+        setLikeCount(likeCount + 1);
+    }
+
     function handleDeleteComment()
     {
         deleteComment(content)
@@ -31,9 +39,9 @@ export function Comment({content, deleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
